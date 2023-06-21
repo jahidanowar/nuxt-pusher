d182789f786c3c22a64d66d76d99730f208ba8e3
jahidanowar <milon5143@gmail.com>
2023-06-21 22:46:29 +0530

    Release

--- build.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineBuildConfig } from "unbuild";
-
-export default defineBuildConfig({
-  failOnWarn: false,
-  clean: true,
-  declaration: true,
-  externals: ["pusher", "pusher-js"],
-  outDir: "dist",
-});
